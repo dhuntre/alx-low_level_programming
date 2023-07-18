99f9e5fa71831010b44768e7095ab39baea232d8
dhuntre <dhuntre@gmail.com>
2023-07-18 12:23:09 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the string "chakib" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "chakib";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
